3c845c267e32f0b7d98983c21812ab9bfb157d6d
OussamaGalaxi <galaxiouss01@gmail.com>
2025-03-06 11:59:12 +0000

    feat : Implementation code pour En utilisant le Framework Spring        - Version XML

--- src/main/java/dao/DaoImpl.java
+++ src/main/java/dao/DaoImpl.java
@@ -3,7 +3,7 @@ package dao;
 public class DaoImpl implements IDao {
     @Override
     public double getData() {
-        System.out.println("Version de base du données :");
+        System.out.println("Version base de données :");
         double temp = 35;
         return temp;
     }
