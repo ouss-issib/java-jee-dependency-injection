231968d041363f4ddea606f19ad9483ca0fbf9e4
ouss issib <oussamaissib@gmail.com>
2025-03-08 14:13:35 +0000

    feat:Ajouter la couche presentation en implementer L injection de dependances Par Instantiation Dynamique et ces captures

---
+++ src/main/java/presentation/Pres2.java
@@ -0,0 +1,27 @@
+package presentation;
+
+import dao.IDao;
+import metier.IMetier;
+import metier.MetierImpl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
+public class Pres2 {
+
+    public static void main(String[] args) throws FileNotFoundException,
+            ClassNotFoundException, InstantiationException, IllegalAccessException,
+            NoSuchMethodException, InvocationTargetException {
+        Scanner scanner = new Scanner(new File("config.txt"));
+        String daoClassName = scanner.nextLine();
+        Class cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.newInstance();
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println("Res = "+metier.calcul());
+    }
+}
